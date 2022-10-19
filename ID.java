1a7e0e258b498ee2a73c66ce5cb358f978965ef5
puabhijith7 <235814@ust-global.com>
2022-10-19 22:00:11 +0530

    7

--- ID.java
+++ ID.java
@@ -5,8 +5,9 @@ public class ID
 public static void main(String[] args)
 {
 Scanner sc=new Scanner(System.in);
+System.out.println("Enter your ID");
 String c=sc.nextLine();
-if(c.charAt(0)=='G' &&c.charAt(1)=='B'&&c.charAt(0)=='L')
+if(c.charAt(0)=='G' &&c.charAt(1)=='B'&&c.charAt(2)=='L')
 {
  System.out.println("Login success");
 }
